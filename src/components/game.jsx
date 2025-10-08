6121dff43300586b24b775aa6e1a4c92425d5dfd
luke-obrien <lukeobrien02@gmail.com>
2025-10-08 17:47:24 +0100

    prep

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -32,7 +32,7 @@ export const Game = () => {
   return (
     <>
       <div className={classes.TitleContainer}>
-        <h1 className={classes.CompanyTitle}>General assembly</h1>
+        <h1 className={classes.CompanyTitle}>GA</h1>
       </div>
       <h3 className={classes.GameTitle}>Snap!</h3>
       <div className={classes.GameContainer}>
